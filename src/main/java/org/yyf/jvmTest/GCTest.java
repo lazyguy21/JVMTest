377eb478a03f3ab0e0f2a56b0756de6751c23313
lazyguy21 <15882041972@sina.cn>
2016-10-10 15:39:51 +0800

    -Xmx1000m -Xms1000m -XX:-UseAdaptiveSizePolicy 观察新生代的搜survivor和eden的比例

--- src/main/java/org/yyf/jvmTest/GCTest.java
+++ src/main/java/org/yyf/jvmTest/GCTest.java
@@ -2,6 +2,7 @@ package org.yyf.jvmTest;
 
 /**
  * Created by tobi on 16-10-9.
+ * -Xmx1000m -Xms1000m -XX:-UseAdaptiveSizePolicy 观察新生代的搜survivor和eden的比例
  */
 public class GCTest {
     private static final Integer _1M = 1024 * 1024;
@@ -10,7 +11,7 @@ public class GCTest {
         Byte[] object;
         while(true){
             object = new Byte[_1M];
-            System.out.println("createNum : "+ ++createNum);
+//            System.out.println("createNum : "+ ++createNum);
         }
     }
 }
